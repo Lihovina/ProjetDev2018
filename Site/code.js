7cd2e7cdf2aad4b0105ca16a1fca268095b8906c
Lihovina <35365718+Lihovina@users.noreply.github.com>
2018-02-14 16:40:25 +0100

    Add files via upload

---
+++ Site/code.js
@@ -0,0 +1,19 @@
+new Vue({
+ el: '#app',
+ data: {
+       items: [
+            {
+              src: '../Images/img5.jpg'
+            },
+            {
+              src: '../Images/img2.jpg'
+            },
+            {
+              src: '../Images/img3.jpg'
+            },
+            {
+              src: '../Images/img4.jpg'
+            }
+        ]
+   }
+})
